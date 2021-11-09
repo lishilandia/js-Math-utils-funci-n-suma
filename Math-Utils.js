30d927343055298aec6625749897882f38779143
Lishilandia <lisette.pavez@gmail.com>
2021-11-09 19:54:19 -0300

    función multiplicación agregrada

--- Math-Utils.js
+++ Math-Utils.js
@@ -8,11 +8,20 @@ suma(20, 7)
 suma(35, 21)
 
 
-function resta(a, b) {
-  console.log(a - b);
-  return a - b
+function resta(c, d) {
+  console.log(d - d);
+  return c - d
 }
 
 resta(15, 5)
 resta(28, 7)
-resta(105,21)
+resta(105, 21)
+
+function multiplicacion(e,f) {
+   console.log(e * f);
+  return e * f
+}
+
+multiplicacion(2, 3)
+multiplicacion(4, 37)
+multiplicacion(76, 98)
